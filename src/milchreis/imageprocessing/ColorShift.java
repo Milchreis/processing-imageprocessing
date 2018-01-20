c93d994f42fb358eae4b7df1671c1d5c839b2425
Nick <544436+Milchreis@users.noreply.github.com>
2018-01-20 20:38:01 +0100

    Adds color shifting (in dev.) and RGB to HSB and vice versa

---
+++ src/milchreis/imageprocessing/ColorShift.java
@@ -0,0 +1,106 @@
+package milchreis.imageprocessing;
+
+import processing.core.PApplet;
+import processing.core.PImage;
+
+/**
+ * 
+ * @author milchreis
+ */
+public class ColorShift {
+	
+	
+	public static PImage apply(PImage image, int shift) {
+		PImage output = image.copy();
+        
+        for (int x = 0; x < image.width; x++) {
+            for (int y = 0; y < image.height; y++) {
+            	
+            	int pixel = image.get(x, y);
+            	float[] hsb = Tools.rgbToHsb(pixel);
+
+        		hsb[0] += shift;
+        		hsb[0] = PApplet.constrain(hsb[0], 0, 360);
+            	
+            	pixel = Tools.hsbToRgb(hsb[0], hsb[1], hsb[2]);
+                output.set(x, y, pixel);
+            }
+        }
+
+        return output;
+	}
+	
+	public static PImage apply(PImage image, int hue, int offset, int shift) {
+		return applyHue(image, hue, offset, shift);
+	}
+	
+    public static PImage applyHue(PImage image, int hue, int offset, int shift) {
+        
+        PImage output = image.copy();
+        
+        for (int x = 0; x < image.width; x++) {
+            for (int y = 0; y < image.height; y++) {
+            	
+            	int pixel = image.get(x, y);
+            	float[] hsb = Tools.rgbToHsb(pixel);
+
+            	if(Tools.in(hue, hsb[0]-offset, hsb[0]+offset)) {
+            		hsb[0] += shift;
+            		hsb[0] = PApplet.constrain(hsb[0], 0, 360);
+            	}
+            	
+            	pixel = Tools.hsbToRgb(hsb[0], hsb[1], hsb[2]);
+                output.set(x, y, pixel);
+            }
+        }
+
+        return output;
+    }
+    
+    public static PImage applySaturation(PImage image, int hue, int offset, float shift) {
+    	
+    	PImage output = image.copy();
+    	
+    	for (int x = 0; x < image.width; x++) {
+    		for (int y = 0; y < image.height; y++) {
+    			
+    			int pixel = image.get(x, y);
+    			float[] hsb = Tools.rgbToHsb(pixel);
+    			
+    			if(Tools.in(hue, hsb[0]-offset, hsb[0]+offset)) {
+    				hsb[1] += shift;
+    				hsb[1] = PApplet.constrain(hsb[1], 0, 1);
+    			}
+    			
+    			pixel = Tools.hsbToRgb(hsb[0], hsb[1], hsb[2]);
+    			output.set(x, y, pixel);
+    		}
+    	}
+    	
+    	return output;
+    }
+    
+    public static PImage applyBrightness(PImage image, int hue, int offset, float shift) {
+    	
+    	PImage output = image.copy();
+    	
+    	for (int x = 0; x < image.width; x++) {
+    		for (int y = 0; y < image.height; y++) {
+    			
+    			int pixel = image.get(x, y);
+    			float[] hsb = Tools.rgbToHsb(pixel);
+    			
+    			if(Tools.in(hue, hsb[0]-offset, hsb[0]+offset)) {
+    				hsb[2] += shift;
+    				hsb[2] = PApplet.constrain(hsb[2], 0, 1);
+    			}
+    			
+    			pixel = Tools.hsbToRgb(hsb[0], hsb[1], hsb[2]);
+    			output.set(x, y, pixel);
+    		}
+    	}
+    	
+    	return output;
+    }
+    
+}
--- src/milchreis/imageprocessing/Tools.java
+++ src/milchreis/imageprocessing/Tools.java
@@ -1,5 +1,8 @@
 package milchreis.imageprocessing;
 
+import java.awt.Color;
+
+import processing.core.PApplet;
 import processing.core.PImage;
 
 /**
@@ -8,26 +11,58 @@ import processing.core.PImage;
  */
 public class Tools {
 
+	public static PImage createImage(int w, int h, int format) {
+		PImage image = new PImage(w, h, format);
+		return image;
+	}
 
-    public static PImage createImage(int w, int h, int format) {
-        PImage image = new PImage(w, h, format);
-        return image;
-    }
-    
-    public static PImage createImage(int w, int h, int format, PImage original) {
-    	PImage image = createImage(w, h, format);
-    	image.parent = original.parent;  // make save() work
-    	return image;
-    }
+	public static PImage createImage(int w, int h, int format, PImage original) {
+		PImage image = createImage(w, h, format);
+		image.parent = original.parent; // make save() work
+		return image;
+	}
 
-    public int[] getArray(int arr[], int val) {
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = val;
-        }
-        return arr;
-    }
+	public int[] getArray(int arr[], int val) {
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = val;
+		}
+		return arr;
+	}
 
 	public static PImage createBlankImageLike(PImage image) {
 		return createImage(image.width, image.height, image.format, image);
+	}
+	
+	public static int[] getRGB(int rgb) {
+		int r = (rgb >> 16) & 0xff;
+		int g = (rgb >> 8) & 0xff;
+		int b = (rgb & 0xff);
+		return new int[]{r, g ,b};
+	}
+
+	public static float[] rgbToHsb(int rgb) {
+		int[] arrRGB = getRGB(rgb);
+		return rgbToHsb(arrRGB[0], arrRGB[1], arrRGB[2]);
+	}
+	
+	public static float[] rgbToHsb(int r, int g, int b) {
+
+		float[] hsb = Color.RGBtoHSB(r, g, b, new float[3]);
+		hsb[0] = PApplet.map(hsb[0], 0f, 1f, 0, 360);
+		
+		return hsb;
+	}
+
+	public static int hsbToRgb(float h, float s, float b) {
+		h = PApplet.map(h, 0f, 360f, 0, 1f);
+		return Color.HSBtoRGB(h, s, b);
+	}
+	
+	public static boolean in(int x, int start, int end) {
+		return in((float)x, start, end);
+	}
+
+	public static boolean in(float x, float start, float end) {
+		return x >= start && x <= end;
 	}
 }
